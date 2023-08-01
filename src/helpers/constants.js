54863db9795fcd759572a3c53c5da49137f15d5d
Radhakishan Jangid <radhakishanjangid404@gmail.com>
2023-08-01 19:19:21 +0530

    api change

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
-const NODE_ENV = "http://62.72.0.219/api"; // local development
+const NODE_ENV = "https://bidsinfoglobal.com/api"; // local development
 
 export const searchType = ["Any Word", "Exact Phrase", "Relevant Word"];
 export const geoLocationKeyword = ["Regions", "Country", "State & City"];
